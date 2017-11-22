94f7bd769565d8ca6c61729555447c66ff35d5c4
kehayov <tihomirkehayov@deltasource.eu>
2017-11-22 21:03:49 +0200

    add persistence

---
+++ importer/app/persistence/products.go
@@ -0,0 +1,15 @@
+package persistence
+
+type Product struct {
+	Description  string
+	Model        string
+	Name         string
+	Kkprice      float64
+	Price        float64
+	PriceDds     float64
+	SmallPicture string
+}
+
+func AddMultiProduct() {
+
+}
